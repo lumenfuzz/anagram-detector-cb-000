bc05f489dd5d7ff4568d9288c4ad732b1cb6ff85
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 18:39:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  @@characters_of_word = []
+  def initialize(word)
+    @@characters_of_word = word.split("").sort!
+  end
+  def self.match(array)
+    anagrams = []
+    array.each do |word|
+      characters = word.split("").sort!
+      anagrams << word if characters == characters_of_word
+    end
+    return anagrams
+  end
+end
