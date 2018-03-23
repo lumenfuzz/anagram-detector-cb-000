5f5ca660d184474a667fadfee18d76882f1dae4a
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-23 18:41:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  @@characters_of_word = []
+
+  def initialize(word)
+    @@characters_of_word = word.split("").sort!
+  end
+
+  def match(array)
+    anagrams = []
+    array.each do |word|
+      characters = word.split("").sort!
+      anagrams << word if characters == @@characters_of_word
+    end
+    return anagrams
+  end
+
+end
